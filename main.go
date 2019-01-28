39da3e9ba1674d38f32e8e75d4cdafd4008f8b99
Josh Bleecher Snyder <josharian@gmail.com>
2019-01-28 10:51:19 -0800

    accept starlark-go flags

--- main.go
+++ main.go
@@ -19,16 +19,43 @@ var impls = [...]struct {
 
 func main() {
 	log.SetFlags(0)
-	if len(os.Args) != 2 {
-		log.Fatal("usage: starbug <prog>")
+	if len(os.Args) < 2 {
+		log.Fatal("usage: starbug [flags] <prog>")
 	}
-	prog := os.Args[1]
+
+	// Attempt to auto-assign flags to the appropriate interpreter.
+	// For now, simply recognize and accept starlark-go flags.
+	flags := os.Args[1 : len(os.Args)-1]
+	validgoflag := map[string]bool{
+		"-bitwise":        true,
+		"-float":          true,
+		"-globalreassign": true,
+		"-lambda":         true,
+		"-nesteddef":      true,
+		"-recursion":      true,
+		"-set":            true,
+		"-showenv":        true,
+	}
+	allflags := make(map[string][]string)
+	for _, s := range flags {
+		if validgoflag[s] {
+			allflags["starlark-go"] = append(allflags["starlark-go"], s)
+		} else {
+			log.Fatal("unrecognized flag", s)
+		}
+	}
+
+	prog := os.Args[len(os.Args)-1]
+
 	for _, impl := range impls {
 		path, err := exec.LookPath(impl.bin)
 		if err != nil {
 			log.Printf("%s not found: %v", err)
 		}
-		cmd := exec.Command(path, "-c", prog)
+		iflags := allflags[impl.name]
+		iflags = iflags[:len(iflags):len(iflags)]
+		args := append(iflags, "-c", prog)
+		cmd := exec.Command(path, args...)
 		out, err := cmd.CombinedOutput()
 		rc := -1
 		if err == nil {
@@ -44,7 +71,11 @@ func main() {
 				quote = ""
 			}
 		}
-		log.Printf("$ %s -c %s%s%s", impl.name, quote, prog, quote)
+		flagstr := ""
+		for _, f := range iflags {
+			flagstr += " " + f
+		}
+		log.Printf("$ %s%s -c %s%s%s", impl.name, flagstr, quote, prog, quote)
 		log.Print(string(out))
 		if rc != -1 {
 			log.Printf("(exit %v)", rc)
